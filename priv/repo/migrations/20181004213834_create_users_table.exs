c01d53c7dc593887ae130ed00df90728e7afeda5
Edgar Pino <edgarbeltran@me.com>
2018-10-05 16:18:24 -0600

    Create users migration

---
+++ priv/repo/migrations/20181004213834_create_users_table.exs
@@ -0,0 +1,11 @@
+defmodule GettingStartedWithEcto.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add(:name, :string)
+
+      timestamps()
+    end
+  end
+end
